17c7b77463d9b03cdc228911b86a2be70845abd1
Ishrak <admin@ishrak.xyz>
2026-09-10 21:14:01 +0000

    Disable incompatible Redis cache drop-in

--- docker-entrypoint-init.sh
+++ docker-entrypoint-init.sh
@@ -5,16 +5,8 @@ if [ ! -f /var/www/html/wp-load.php ]; then
     cp -a /usr/src/wordpress/. /var/www/html/
 fi
 
-if [ ! -f /var/www/html/wp-config.php ] && [ -f /var/www/html/wp-content/object-cache.php ]; then
+if [ -f /var/www/html/wp-content/object-cache.php ]; then
     mv /var/www/html/wp-content/object-cache.php /var/www/html/wp-content/object-cache.php.disabled
-fi
-
-if [ -f /var/www/html/wp-config.php ] && [ -f /var/www/html/wp-content/object-cache.php.disabled ]; then
-    mv /var/www/html/wp-content/object-cache.php.disabled /var/www/html/wp-content/object-cache.php
-fi
-
-if [ -f /var/www/html/wp-config.php ] && ! grep -q "WP_REDIS_HOST" /var/www/html/wp-config.php; then
-    sed -i "/That's all, stop editing/i\\define('WP_REDIS_HOST', 'cache');\ndefine('WP_REDIS_PORT', 6379);\ndefine('WP_REDIS_DATABASE', 0);\ndefine('WP_REDIS_TIMEOUT', 1);\ndefine('WP_REDIS_READ_TIMEOUT', 1);\n" /var/www/html/wp-config.php
 fi
 
 chown -R www-data:www-data /var/www/html
